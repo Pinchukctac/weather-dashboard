b47d81dd48dc747689cc5411c2685b6c15605d32
github_Pinchukctac <pinchukctac11@gmail.com>
2020-05-27 15:58:06 -0400

    added psuedo code

---
+++ 02-Homework/02-Homework/script.js
@@ -0,0 +1,10 @@
+// make variables for input, cities search history
+// click event button
+// funtion that is used when we click button to call API 
+// fuction argument cities names 
+// display info thats pulled from api (append to HTML)
+// if statement to push a css class to UV index based on range numbers
+// create array that checks if search is already in it, if not then it adds to the array. saves it to local storage. limit number to 5 previous searches. check the array.length. (array.push "value"). if already 5 long, delete the first one "look up syntax". pull array local storage on refresh and pull last search from array and get info from API and dispay. 
+// (.epmty). when clicking search button, deletes info from the HTML so that oit only show info from the new search. 
+
+//  think how/why when you think of every step. 
