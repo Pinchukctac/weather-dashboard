cea94836b28b2a2130c9ab31b37ea41b78654727
github_Pinchukctac <pinchukctac11@gmail.com>
2020-06-04 19:47:04 -0400

    fixed local storage not showing up

--- script.js
+++ script.js
@@ -15,73 +15,28 @@ var storedCities = [];
 $("#search-button").on("click", function(event){
     event.preventDefault();
     var input = $("#search-input").val();
-    var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + input + "&appid=e9a9052aaa132988695d1b11382ed400"
-
-    
-    // Ajax call
-    $.ajax({
-        url: queryURL,
-        method: "GET"
-    })
-
-    //  Function to pull current weather and append it to the HTML file
-    .then(function(response){
-
-            $('#city-name').empty();
-
-            lat = response.coord.lat
-            long = response.coord.lon
-
-            var temp = (response.main.temp - 273.15) * 1.80 + 32;    
-            var tempF = (Math.floor(temp))
-            var city = $("#city-name").addClass("card-title").text(response.name);
-            var temperature = $(".row1").addClass("card-text current-temp").text("Temperature: " + tempF + " °F");
-            var humidity = $(".row2").addClass("card-text current-humidity").text("Humidity: " + response.main.humidity + "%");
-            var wind = $(".row3").addClass("card-text current-wind").text("Wind Speed: " + response.wind.speed + " MPH");
-            var image = $("<img>").attr("src", "https://openweathermap.org/img/w/" + response.weather[0].icon + ".png")
-            
-
-            $(city).append(image)
-            saveLocation(input)
-
-    })
-
-    // function that pulls the UV index
-    .then(function(event2){
-
-        var uvQueryURL = "https://api.openweathermap.org/data/2.5/uvi?appid=e9a9052aaa132988695d1b11382ed400&lat="+lat+"&lon="+long
-
-        $.ajax({
-            url: uvQueryURL,
-            method: "GET"
-        })
-    
-        .then(function(response2){  
-
-            
-
-           uvIndex = $(".row4").addClass("card-text current-UV").text("UV Index: " + response2.value); 
-
-           if(response2.value >= 10){
-            uvIndex.addClass("red")
-            }
-            else if( response2.value < 10 || response2.value >= 4){
-                uvIndex.addClass("orange")
-            }
-            else {
-                uvIndex.addClass("green")
-            }
-        
-        })
-    })
+    getWeather(input)
+   
 });
 
+
+$(".saved-city").on("click", function(event){
+    event.preventDefault();
+    var oldCities = $(this).text()
+    console.log(oldCities)
+    getWeather(oldCities)
+   
+});
 
 
 // On click event for 5 day forcast 
 $("#search-button").on("click", function(event){
     event.preventDefault();
     var input = $("#search-input").val();
+
+
+
+    
     var queryURL = "https://api.openweathermap.org/data/2.5/forecast?q=" + input + "&appid=e9a9052aaa132988695d1b11382ed400"
 
 
@@ -133,19 +88,21 @@ function saveLocation(input){
     }
     //save the new array to localstorage
     localStorage.setItem("city", JSON.stringify(storedCities));
+    savedStorage();
 }
 
 // retrieve searches from local storage
 function savedStorage() {
     //grab previous locations from local storage
-    savedcities = JSON.parse(localStorage.getItem("city"));
+    savedCities = JSON.parse(localStorage.getItem("city"));
      //display buttons for previous searches
-    if (savedcities) {
+    if (savedCities) {
         //get the last city searched so we can display it
         currentLoc = savedCities[savedCities.length - 1];
         showPrevious();
 
     }
+    console.log(savedCities)
 }
     
 // Display the cities from local storage
@@ -155,12 +112,12 @@ function showPrevious() {
         $("#history").empty();
             var btns = $("<div>").attr("class", "list-group");
              for (var i = 0; i < savedCities.length; i++) {
-                var locBtn = $("<a>").attr("href", "#").attr("id", "loc-btn").text(savedCities[i]);
+                var locBtn = $("<button>").attr("id", "loc-btn").text(savedCities[i]);
                  if (savedCities[i] == currentLoc){
-                    locBtn.attr("class", "list-group-item list-group-item-action active");
+                    locBtn.attr("class", "list-group-item list-group-item-action active saved-city");
                 }
                 else {
-                    locBtn.attr("class", "list-group-item list-group-item-action");
+                    locBtn.attr("class", "list-group-item list-group-item-action saved-city");
                 }
                 btns.prepend(locBtn);
         }
@@ -168,3 +125,72 @@ function showPrevious() {
     }
 }
         
+savedStorage();
+
+
+function getWeather(input){
+    var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + input + "&appid=e9a9052aaa132988695d1b11382ed400"
+
+    
+    // Ajax call
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+    })
+
+    //  Function to pull current weather and append it to the HTML file
+    .then(function(response){
+
+            $('#city-name').empty();
+
+            lat = response.coord.lat
+            long = response.coord.lon
+
+            var temp = (response.main.temp - 273.15) * 1.80 + 32;    
+            var tempF = (Math.floor(temp))
+            var city = $("#city-name").addClass("card-title").text(response.name);
+            var temperature = $(".row1").addClass("card-text current-temp").text("Temperature: " + tempF + " °F");
+            var humidity = $(".row2").addClass("card-text current-humidity").text("Humidity: " + response.main.humidity + "%");
+            var wind = $(".row3").addClass("card-text current-wind").text("Wind Speed: " + response.wind.speed + " MPH");
+            var image = $("<img>").attr("src", "https://openweathermap.org/img/w/" + response.weather[0].icon + ".png")
+            
+
+            $(city).append(image)
+            saveLocation(input)
+
+
+    })
+
+    // function that pulls the UV index
+    .then(function(event2){
+
+        var uvQueryURL = "https://api.openweathermap.org/data/2.5/uvi?appid=e9a9052aaa132988695d1b11382ed400&lat="+lat+"&lon="+long
+
+        $.ajax({
+            url: uvQueryURL,
+            method: "GET"
+        })
+    
+        .then(function(response2){  
+
+            
+            
+           uvIndex = $(".row4")
+           uvIndex.empty()
+           uvIndex.removeClass("red orange green")
+           uvIndex.addClass("card-text current-UV").text("UV Index: " + response2.value); 
+
+           if(response2.value >= 10){
+            uvIndex.addClass("red")
+            }
+            else if( response2.value < 10 || response2.value >= 4){
+                uvIndex.addClass("orange")
+            }
+            else {
+                uvIndex.addClass("green")
+            }
+        
+        })
+    })
+
+}
